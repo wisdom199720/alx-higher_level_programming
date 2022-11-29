e409d273efe454d78f17d0f2845dc7989d1129d6
wisdom199720 <agboighalewisdom@gmail.com>
2022-11-29 11:07:29 -0800

    new

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for j in range(97, 123):
+    print("{}".format(chr(j)), end="")
+    Footer
+     2022 GitHub, Inc.
+     Footer navigation
+     Terms
+     Privacy
+     Security
+     Status
+     Docs
+     Contact GitH
