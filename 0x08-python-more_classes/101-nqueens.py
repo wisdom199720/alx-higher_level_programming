fb76db544914bdb6d4bd9c8f3ecf8c995121bff1
wisdom199720 <agboighalewisdom@gmail.com>
2023-01-04 04:47:12 -0800

    new

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -14,6 +14,7 @@ queen must be placed on the chessboard.
 """
 import sys
 
+
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
@@ -21,17 +22,19 @@ def init_board(n):
     [row.append(' ') for i in range(n) for row in board]
     return (board)
 
+
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
 
+
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
     solution = []
     for r in range(len(board)):
-        for c in range(len(board)): 
+        for c in range(len(board)):
             if board[r][c] == "Q":
                 solution.append([r, c])
                 break
@@ -87,6 +90,7 @@ def xout(board, row, col):
             break
         board[r][c] = "x"
         c -= 1
+
 
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
